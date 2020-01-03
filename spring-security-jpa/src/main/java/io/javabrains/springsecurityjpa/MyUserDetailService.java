95b395a68919c1629b0238aef74bbb0ed0d47083
Shadow Song <easonback@gmail.com>
2020-01-02 21:20:18 -0500

    upload

---
+++ spring-security-jpa/src/main/java/io/javabrains/springsecurityjpa/MyUserDetailService.java
@@ -0,0 +1,26 @@
+package io.javabrains.springsecurityjpa;
+
+import io.javabrains.springsecurityjpa.models.MyUserDetails;
+import io.javabrains.springsecurityjpa.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public MyUserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found :" + userName));
+
+        return user.map(MyUserDetails::new).get();
+    }
+}
