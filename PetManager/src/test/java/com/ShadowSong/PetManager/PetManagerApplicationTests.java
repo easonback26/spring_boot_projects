c4b1df1109f54a5196ebdb44929c6763fac379dc
Shadow Song <easonback@gmail.com>
2020-01-03 20:29:50 -0500

    Add PetManager

---
+++ PetManager/src/test/java/com/ShadowSong/PetManager/PetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShadowSong.PetManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
